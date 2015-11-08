6b53428d51584a50a385c1aebb5a3af00723ca4d
wings <wings@wings-Inspiron-5559>
2015-11-08 15:53:15 -0500

    SDFSD

--- fso_project/final_fso/src/test/test_map_to_graph_generator.py
+++ fso_project/final_fso/src/test/test_map_to_graph_generator.py
@@ -3,16 +3,21 @@ from final_fso.src.map_to_graph_generator import MapToGraphGenerator
 
 class TestMapToGraphGenerator(unittest.TestCase):
   def setUp(self):
-    mapFilePath = './maps/tiny_nyc.osm'
-    outputFilePath = './maps/graphs/tiny_nyc_graph.txt'
+    filename = 'manhattan'
+    mapFilePath = './maps/'+filename+'.osm' 
+    building_outputFilePath = './maps/graphs/'+filename+'.bldg.txt'
+    graph_outputFilePath = './maps/graphs/'+filename+'.adj.txt'
     short_edge_length = 100
-    long_edge_length = 1000 
-    visualize_graph = True
+    long_edge_length = 1000  
+    edge_append_mode = False
+    visualize_graph = True #change this one if needed
     unittest.TestCase.setUp(self)
     self.m2gg = MapToGraphGenerator(mapFilePath, 
-                                    outputFilePath, 
+                                    building_outputFilePath,
+                                    graph_outputFilePath,
                                     short_edge_length, 
                                     long_edge_length, 
+                                    edge_append_mode,
                                     visualize_graph)
     print "Test for MapToGraphGenerator....."
     
